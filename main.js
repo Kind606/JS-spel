63246bed4df0d85529d4f59daf801fb30f1f0856
Kind606 <kevin.hellgren@hotmail.com>
2024-11-05 09:25:39 +0100

    Generate buttons

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function loadStartingSecne() {
     knapp2.onclick = loadGoToCarScene;
     document.getElementById("backGround").style.backgroundImage = "url('Realspökhus.jpg')";
     document.querySelector(".choice-text").innerText = "Du står utanför ett varmt och mysigt hotell, det börjar bli kallt. Vad gör du?";
+    
 }
 
 function loadHotelHallScene() {
@@ -18,14 +19,28 @@ function loadHotelHallScene() {
     knapp2.onclick = loadStartingSecne;
     document.getElementById("backGround").style.backgroundImage = "url('Hall.webp')";
     document.querySelector(".choice-text").innerText = "Hej, detta är ett test.";
-    knapp1.innerText = "Val 1"
-    knapp2.innerText = "Val 2"
+    const btn = document.createElement("button");
+    btn.innerHTML = "testknapp";
+    const choices = document.querySelector(".choices")
+    choices.appendChild(btn);
+    knapp1.innerText = "Val 1";
+    knapp2.innerText = "Val 2";
 }
  
 function loadGoToCarScene() {
-
+    knapp1.onclick = loadLeaveScene;
+    knapp2.onclick = loadStartingSecne;
+    document.getElementById("backGround").style.backgroundImage = "url('Hall.webp')";
+    document.querySelector(".choice-text").innerText = "Hej, detta är ett test.";
+    knapp1.innerText = "Val 1"
+    knapp2.innerText = "Val 2"
 }
 
 function loadKitchenScene() {
-
-}
+    knapp1.onclick = loadLoafScene;
+    knapp2.onclick = loadHotelHallScene;
+    document.getElementById("backGround").style.backgroundImage = "url('Hall.webp')";
+    document.querySelector(".choice-text").innerText = "Hej, detta är ett test.";
+    knapp1.innerText = "Val 1"
+    knapp2.innerText = "Val 2"
+}   
